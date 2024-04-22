1a8386b88864e6d00ae13dcbbd4b8984f79f76f0
Hubba2010 <Hubert.Barecki@onet.pl>
2024-04-22 23:55:32 +0200

    Added mongoose

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Subscription } from 'rxjs';
+import {
+  Component,
+  OnDestroy,
+  OnInit
+} from "@angular/core";
+import {Subscription} from 'rxjs';
 
-import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
+import {Post} from "../post.model";
+import {PostsService} from "../posts.service";
 
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
-  styleUrls: ["./post-list.component.css"]
+  styleUrls: ["./post-list.component.css"],
 })
 export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
@@ -18,7 +22,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   private postsSub: Subscription = new Subscription();
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+  ) {}
 
   ngOnInit() {
     this.postsService.getPosts();
