ada4fc8f05469186d5611b76041ec1833caef499
Hubba2010 <Hubert.Barecki@onet.pl>
2024-04-26 21:30:28 +0200

    User authentication, implemented JWT

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
 const router = express.Router();
 
 router.post('/signup', (req,res,next) => {
-    bcrypt.hash(req.body.password, 20)
+    bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
                 email: req.body.email,
@@ -25,7 +26,38 @@ router.post('/signup', (req,res,next) => {
                     })
                 })
         });
+});
 
-})
+router.post('/login', (req,res,next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email})
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Auth failed!'
+                });
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: 'Auth failed!'
+                });
+            }
+            const token = jwt.sign(
+                {email: fetchedUser.email, userId: fetchedUser._id},
+                'this_should_be_a_long_secret         ',
+                {expiresIn: 3600}
+            );
+            res.status(200).json({token})
+        })
+        .catch(err => {
+            return res.status(401).json({
+                message: 'Auth failed!'
+            });
+        })
+});
 
 module.exports = router;
